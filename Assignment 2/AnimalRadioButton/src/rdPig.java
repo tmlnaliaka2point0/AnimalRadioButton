1d9fb76289969b98c316cb227ceae1d205b78f24
tmlnaliaka2point0 <sophynaliaka@zetech.ac.ke>
2025-07-04 01:37:16 +0300

    Assignment 2 FOR OOP 2

---
+++ Assignment 2/AnimalRadioButton/src/rdPig.java
@@ -0,0 +1,19 @@
+
+import java.awt.event.ActionListener;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Naliaka
+ */
+class rdPig {
+
+    static void addActionListener(ActionListener listener) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
